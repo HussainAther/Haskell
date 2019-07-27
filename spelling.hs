1f85124d5ec7b5b3cee18aaad7d1e045fe818103
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 19:58:35 -0400

    import

--- spelling.hs
+++ spelling.hs
@@ -1,1 +1,6 @@
+import Data.Char (isAlpha, isSpace, toLower)
+import Data.List (group, sort, maximumBy)
+import Data.Ord (comparing)
+import Data.Map (fromListWith, Map, Member, (!))
+
 -- Fix spelling errors.
