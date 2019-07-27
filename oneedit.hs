34f462fa51152ab6d2a985699a059dbcec7273fb
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 17:53:19 -0400

    edits

--- oneedit.hs
+++ oneedit.hs
@@ -2,3 +2,12 @@ import Data.Char (toLower)
 import Data.List (group, sort)
 
 -- Find strings one-edit distance from a string.
+
+edits1 :: String -> [String]
+edits1 word = unique $ deletes ++ transposes ++ replaces ++ inserts
+  where splits = [(take i word', drop i word')|
+    i <- [i..length word']]
+
+deletes = [a ++ (tail b) |
+  (a, b) <- splits, (not.null) b]
+
