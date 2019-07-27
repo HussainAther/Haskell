16f30b59b381e5b3cec4ad9a87dea46912040a75
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 04:11:22 -0400

    run it

--- filehandle.hs
+++ filehandle.hs
@@ -1,1 +1,13 @@
 import System.IO(Handle, FilePath, IOMode( ReadMode ), openFile, hGetLine, hPutStr, hClose, hIsEOF, stderr)
+import Control.Monad(when)
+
+dumpeFile :: Handle -> FilePath -> Integer -> IO ()
+
+dumpFile handle filename lineNumber = do 
+    end <- hIsEOF handle
+    while ( not end ) $ do
+        line <- hGetLine handle
+        putStrLn $ filename ++ ":" ++ show lineNumber ++ ": " ++ line
+        dumpeFile handle filename $ lineNumber + 1
+
+main :: IO ()
