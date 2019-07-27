b62a145b148cda8a2ae07366fc9dc3dfdd9e759c
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 17:47:40 -0400

    func

--- levenshtein.hs
+++ levenshtein.hs
@@ -1,1 +1,11 @@
 import qualified Data.MemoCombinators as Memo
+
+lev :: Eq a => [a] -> [a] -> Int
+lev a b = levM (length a) (length b)
+  where levM = memoize lev'
+    lev' i j
+      | min i j == o = max i j
+      | otherwise = minimum
+        [(1 + levM (i-1) j)
+         , (1 + levM i (j-1)
+         , ( ind i j levM (i-1) (j-1) ] 
