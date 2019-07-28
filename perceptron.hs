078326d735a0561579b65ced5fbd41edb12b8960
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 21:46:05 -0400

    init var

--- perceptron.hs
+++ perceptron.hs
@@ -1,2 +1,14 @@
 import Control.Monad (replicateM)
 import System.Random (randomR, getStdRandom)
+
+--Neural network perceptron.
+
+type Inputs = [Float]
+type Weights = [Float]
+type Threshold = Float
+type Output = Float
+type Expected = Float
+type Actual = Float
+type Delta = Float
+type Interval = Int
+type Step = (Weights, Interval)
