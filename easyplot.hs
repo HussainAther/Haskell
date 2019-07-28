a9e665f6e6692a9163e4df2782fb9276247dfb2a
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 00:36:16 -0400

    error handling

--- easyplot.hs
+++ easyplot.hs
@@ -8,3 +8,9 @@ tupes records = [(read x, read y) | [x, y] <- records]
 
 main = do
   result <- parseCSVFromFile "input.csv"
+
+case result of 
+  Left err -> putStrLn "Error reading CSV file"
+  Right csv -> do
+    plot X11 $ Data3D [Title "Plot"] [] (tupes csv)
+      return ()
