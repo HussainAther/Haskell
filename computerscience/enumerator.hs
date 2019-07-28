5961eafdfacbf58b8c14b5d5e706b743ef619164
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 01:24:06 -0400

    init

--- computerscience/enumerator.hs
+++ computerscience/enumerator.hs
@@ -1,2 +1,15 @@
 --Loop through chunks of a file and accumulate lines. The enumerator feeds data to an
 --iteratee to get a result.
+
+enumerateFile path initIter = 
+  withFile path Readmode $ \h
+    let 
+    go iter = do
+      isEOF <- HIsEOF h
+      if isEOF
+        then return (HaveLine "End of File" "")
+        else do
+          chunk <- B.hGet h 8
+          check $
+      runIter iter chunk check (NeedChunk iterNext)
+
