6508ea20cb5fc223dabbd5a1f29bbc04d8c98ca1
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 20:01:15 -0400

    map

--- spelling.hs
+++ spelling.hs
@@ -4,3 +4,13 @@ import Data.Ord (comparing)
 import Data.Map (fromListWith, Map, Member, (!))
 
 -- Fix spelling errors.
+
+autofix :: Map String Int -> String -> String
+autofix m sentence = unwords $ map (correct m) (words sentence)
+
+getWords :: String -> [String]
+getWords str = words $ filter (\x -> isAlpha || isSpace x) lower
+where lower = map toLower str
+
+train :: [String] -> Map String Int
+train = fromListWith (+) . ('zip' repeat 1)
