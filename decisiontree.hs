07024adb8ee2ad9e12e800e538d0be420375a39b
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 00:05:00 -0400

    entropies

--- decisiontree.hs
+++ decisiontree.hs
@@ -26,4 +26,15 @@ samples d = map fst d
 classes :: DataSet -> [Class]
 classes d = map snd d
 
-entropy 
+entropy :: (Eq a) => [a] -> Entropy
+entropy xs = sum $ map (\x -> prob x * info x) $ nub xs 
+  where prob x = (length' (elemIndices x xs)) / (length' xs)
+    info x = nega $ logBase 2 (prob x)
+    length' xs = fromIntegral $ length xs
+
+splitAttr :: [(Feature, Class)] -> Map Feature [Class]
+splitAttr fc = foldl (\m (f,c) -> M.insertWith (++) f[c]m)
+  M.empty fc
+
+splitEntropy :: Map Feature [Class] -> M.Map Feature Entropy
+splitEntropy m = M.map entropy m 
