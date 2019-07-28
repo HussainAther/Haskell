b52466c4f091cd644ec2cfc0968e7198072ab3b5
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 00:24:21 -0400

    iptonum

--- knn.hs
+++ knn.hs
@@ -5,3 +5,8 @@ import qualified Data.Map as M
 import Data.Maybe (fromJust)
 
 --K-nearest neighbors classifier classification (k nearest neighbros knearest neighbors knn)
+
+ipToNum :: String -> Double
+ipToNum str = fromIntegral $ sum $
+  zipWith (\a b -> a * 256^b) ns [o..]
+  where ns = reverse $ fromIPv4 (read str :: IPv4)
