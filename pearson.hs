833f6e58af1a54a1eb3877428d610c56b3db65b3
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 21:11:29 -0400

    check

--- pearson.hs
+++ pearson.hs
@@ -3,3 +3,19 @@ import Text.CSV
 import Data.List (tails, nub, sort)
 
 --Pearson coefficient.
+
+calcSimilarities (Left error) = error "error parsing"
+calcSimilarities (Right csv) = head $ reverse $ sort $ zip
+  [ pearson (covertList a) (convertList) b)
+  | (a, b) <- pairs csv]
+  $ (pairs csv)
+
+convertList :: [String] -> [Double]
+convertList = map read
+pairs xs = [(x, y) | (x:ys) <- tails (nub xs), y <- ys]
+
+main = do 
+  let fileName = "ratings.csv"
+  input <- readFile filename
+  let csv = parseCSV fileName input
+  print $ calcSimilarities csv
