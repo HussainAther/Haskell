c27177de494f39d92a5b465abab905b229d05f60
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 00:53:58 -0400

    main

--- manhattan.hs
+++ manhattan.hs
@@ -1,3 +1,10 @@
 import Text.CSV (parseCSV)
 
 --Calculate Manhattan distance.
+
+main :: IO ()
+main = do
+  let fileName = "input.csv"
+  input <- readFile fileName
+  let csv = parseCSV fileName input
+
