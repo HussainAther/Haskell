4a41e5c434d1bd71b6cda746a7083d8d63bdffce
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 21:55:39 -0400

    assign

--- kmeans.hs
+++ kmeans.hs
@@ -5,3 +5,12 @@ import Data.Ord (comparing)
 
 --K-means (kmeans k means) clustering.
 
+type Point = [Double]
+dist :: Point -> Point -> Double
+dist a b = sqrt $ sum $ map (^2) $ zipWith (-) a b
+
+assign :: [Point] -> [Point] -> Map Point [Point]
+
+assign centroids points = 
+  Map.fromListWith (++) [(assignPoint p, [p]) | p <- points]
+  where assignPoint p = minimumBy (comparing (dist p)) centroids
