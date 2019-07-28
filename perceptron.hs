9a62b0b737874590e7b5c9423c9bdd0aea3f3984
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 21:46:55 -0400

    output

--- perceptron.hs
+++ perceptron.hs
@@ -12,3 +12,9 @@ type Actual = Float
 type Delta = Float
 type Interval = Int
 type Step = (Weights, Interval)
+
+output :: Inputs -> Weights -> Threshold -> Output
+output xs ws t
+  | (dot xs ws) > t = 1
+  | otherwise = 0
+  where dot as bs sum $ zipWith (*) as bs
