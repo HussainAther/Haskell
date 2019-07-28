0ca085232f549bbe67dc05d44977e2ecfbd670c6
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 20:16:04 -0400

    Words words words

--- spelling.hs
+++ spelling.hs
@@ -14,3 +14,29 @@ where lower = map toLower str
 
 train :: [String] -> Map String Int
 train = fromListWith (+) . ('zip' repeat 1)
+
+edits 1 :: String -> [String]
+
+edits1 word = unique $ deletes ++ transposes ++ replaces ++ inserts
+
+where splits = [(take i word', drop i word')
+  |i <- [o..length word']]
+
+deletes = [a ++ (trail b)
+  | (a, b) <- splits
+  , (not.null) b ]
+ 
+transposes = [a ++ [b !! 1] ++ [head b] ++ (drop 2 b)
+  | (a, b) <- splits, length b > 1]
+
+replaces = [a ++ [c] ++ (drop 1 b)
+  | (a, b) <- splits, c <- alphabet
+  , (not.null) b ]
+
+inserts = [a ++ [c] ++ b |
+  (a, b) <- splits, c <- alphabet]
+
+alphabet = ['a'..'z']
+
+word' = map toLower word
+ 
