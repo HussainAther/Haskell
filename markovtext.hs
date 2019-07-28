3085446d2caeff4f028d63f0a251882092ff7656
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 21:37:56 -0400

    putstr

--- markovtext.hs
+++ markovtext.hs
@@ -3,3 +3,10 @@ import System.Random (mkStdGen)
 
 --Use Markov Chain to generate text.
 
+main = do
+rawText <- readFile "big.txt"
+let g = mkStdGen 100
+putStrLn $ "Character by character: \n"
+putStrLn $ take 100 $ run 3 rawText o g
+putStrLn $ "\nWord by word: \n"
+putStrLn $ unwords $ take 100 $ run 2 (words rawText)o g
