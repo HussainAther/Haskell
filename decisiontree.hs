8d01d3fb214da3f8ed229a4575f67ef30c03b15a
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 00:01:02 -0400

    classes

--- decisiontree.hs
+++ decisiontree.hs
@@ -15,3 +15,15 @@ main = do
   rawCSV <- parseCSVFromFile "input.csv"
   either handleError doWork rawCSV
 handleError = error "invalid file"
+
+doWork csv = do
+  let removeInvalids = filter (\x -> length x > 1)
+  let myData = map(\x -> (init x, last x)) $ removeInvalids csv
+  print $ dtree "root" myData
+
+samples :: DataSet -> [[String]]
+samples d = map fst d
+classes :: DataSet -> [Class]
+classes d = map snd d
+
+entropy 
