e9d2ad701be962b02584935e346ebefac73deb6d
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 22:02:45 -0400

    combine clusters

--- clustering.hs
+++ clustering.hs
@@ -4,3 +4,13 @@ import Data.Ord (comparing)
 import Data.List (sort, tails, transpose, minumumBy)
 
 --Hierarchical clustering
+
+type Point = [Double]
+center :: [Point] -> Point
+center points = map average (transpose points)
+  where xs = sum xs / fromIntegral (length xs)
+
+merge :: Map Point [Point] -> Map Point [Point]
+merge m = Map.insert (center [a, b]) ((m ! a) ++ (m ! b)) newM
+
+where (a, b) = nearest (MAp.keys m)
