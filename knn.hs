fa718eba45a4b717b9753c0135646e7f129959c5
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 00:23:05 -0400

    import

--- knn.hs
+++ knn.hs
@@ -1,1 +1,7 @@
+import Data.Trees.KdTree
+import Data.IP (IPv4, fromIPv4)
+import Text.CSV
+import qualified Data.Map as M
+import Data.Maybe (fromJust)
+
 --K-nearest neighbors classifier classification (k nearest neighbros knearest neighbors knn)
