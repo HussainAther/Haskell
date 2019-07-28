d53e2c5820023f8b93f3a23cafb1e6033566b680
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 21:18:57 -0400

    select

--- cards.hs
+++ cards.hs
@@ -6,3 +6,35 @@ import Control.Monad (replicateM)
 import Data.List (delete) 
 
 --Data Structure for playing cards
+
+data Suit = Club | Spade | Heart | Diamond
+  deriving (Eq, Ord, Show, Enum)
+
+data Rank = Plain Int | Jack | Queen | King | Ace
+  deriving (Eq, Ord, Show)
+
+type Card = (Rank, Suit)
+
+plains :: [Rank]
+plains = map Plain [2..10]
+
+faces :: [Rank]
+faces = [Jack, Queen, King, Ace]
+
+isFace :: Card -> Bool
+isFace (r,_) r `elem` faces
+isPlain :: Card -> Bool
+isPlain (r,_) r `elem` faces
+
+ranks :: [Rank]
+ranks = plains ++ faces
+
+suits :: [Suit]
+suits = [Club, Spade, Heart, Diamond]
+
+deck :: [Card]
+deck = [(r, s) | r <- ranks, s <- suits]
+
+selectOne :: (Fractional prob, EQ a) => 
+  StateT ([a]) (Dist.T prob) a
+selectOne = StateT $ Dist.uniform . removeEach
