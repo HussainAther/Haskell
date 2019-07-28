b4a8888c66ff2e226618b6c10a4ed6803d1d5f43
Syed Hussain Ather <shussainather@gmail.com>
2019-07-28 00:34:34 -0400

    main

--- easyplot.hs
+++ easyplot.hs
@@ -4,3 +4,7 @@ import Graphics.EasyPlot
 --Visualize data points.
 
 tupes :: [[String]] -> [(Double, Double)]
+tupes records = [(read x, read y) | [x, y] <- records]
+
+main = do
+  result <- parseCSVFromFile "input.csv"
