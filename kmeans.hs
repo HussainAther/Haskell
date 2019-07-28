b00294b39496656622a0aa813a938671417b46c0
Syed Hussain Ather <shussainather@gmail.com>
2019-07-27 21:56:44 -0400

    Relocate

--- kmeans.hs
+++ kmeans.hs
@@ -14,3 +14,12 @@ assign :: [Point] -> [Point] -> Map Point [Point]
 assign centroids points = 
   Map.fromListWith (++) [(assignPoint p, [p]) | p <- points]
   where assignPoint p = minimumBy (comparing (dist p)) centroids
+
+relocate :: Map Point [Point] -> Map Point [Point]
+
+relocate centroidsMap = 
+  Map.foldWithKey insertCenter Map.empty centroidsMap
+  where insertCenter _ ps m = Map.insert (center ps) ps m
+    center [] = [0, 0]
+    center ps = map average (transpose ps)
+    average xs = sum xs / fromIntegral (length xs) 
