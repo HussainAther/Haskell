bdb0fa9292481df7a796e86c9e5f3c775195b4db
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 01:04:36 -0400

    where

--- gui/OX.hs
+++ gui/OX.hs
@@ -21,3 +21,18 @@ setSquare :: Board -> Square -> Token -> Board
 setSquare board square token = 
   if (board ! square) /= None then error $ "square " ++ show square ++ " is not empty" 
   else board // [(square, token)]
+
+-- |Determine if the 'Board' is in an end state. 
+-- Returns 'Just' 'Token' if the game has been won, 'Just' 'None' for a draw, 
+otherwise 'Nothing'. 
+endGame :: Board -> Maybe Token 
+endGame board 
+  | Just X `elem` maybeWins = Just X  
+  | Just O `elem` maybeWins = Just O 
+  | None `notElem` elems board = Just None 
+  | otherwise = Nothing 
+  where rows :: [[Square]] 
+    rows = let i = [1..3] 
+      in [[(x,y) | y <- i] | x <- i] ++ -- rows 
+         [[(y,x) | y <- i] | x <- i] ++ -- coloumns 
+         [[(x,x) | x <- i], [(x,4-x) | x <- i]] -- diagonals
