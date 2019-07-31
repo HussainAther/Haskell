8ad5b15ad2456278ca7bae3a22c05752b226046e
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 03:00:00 -0400

    set

--- gui/WxHaskell.hs
+++ gui/WxHaskell.hs
@@ -3,4 +3,13 @@ import Graphics.UI.WX
 
 import OX
 
-
+main = strart $ do
+     --create a new game
+     game <- varCreate newGame
+     -- create the main window
+     window <- frame [text := "OX"]
+     label <- staticText window [text := "Move: X"]
+     btns <- replicateM 3 $ button window [size := sz 40 40]
+     radios <- replicateM 3 $ radioBox window Vertical ["", "?"] []
+     checks <- replicateM 3 $ checkBox window [text := " "] --reserve space for X/O in label
+     set window [layout := column 5 [grid 1 1 [map widget bts, map widget radios, map widget checks], floatCenter $ widget label]]
