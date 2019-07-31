4cf23d3a88f2afef427386a84dc21d9247e4e95e
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 02:27:13 -0400

    gui

--- gui/Gtk2HS.hs
+++ gui/Gtk2HS.hs
@@ -29,3 +29,26 @@ main = do
        mapM_ (boxPackStartDefaults group) [button, button_]
      checks <- replicateM 3 checkButtonNew
      --attach event handlers
+     let event square button = button `on` toggled $ do
+             g@(Game player _) <- readIORef game
+             set button [buttonLabel := show player]
+             let (g'@(Game player _), result) = move g square
+             case result of
+               Nothing -> do --continue game
+                          writeIORef game g'
+                          set button [widgetSensitive := False]
+                          set label [labelLabel := "Move: " ++ show player]
+               Just token -> do --end game
+                             let message = case token of 
+                                                X -> "X won!"
+                                                O -> "O won!"
+                                                None -> "Draw!"
+                             messageDialogNew Nothing [] MessageInfo ButtonsOk message >>= dialogRun
+                             widgetDestroy window
+     zipWithM_ (\b x -> event (x, 1) b >> tableAttachDefaults table b (x-1) x 0 1) btns [1..3]
+     zipWithM_ (\(t,g) x -> event (x,2) r >> tableAttachDefaults table g (x-1) x 1 2) radios [1..3]
+     zipWithM_ (\c x -> event (x,3) c >> tableAttachDefaults table c (x-1) x 2 3) checks [1..3]
+     --run the UI
+     widgetShowAll window
+     mainGUI 
+      
