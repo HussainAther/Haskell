47a4a7b6c97af708432716498c08eab6478a1997
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 01:01:19 -0400

    moar

--- gui/OX.hs
+++ gui/OX.hs
@@ -2,3 +2,22 @@ module OX (Square, Token(..), showBoard, Game(..), newGame, move) where
 
 import Data.Array 
 import Data.List
+
+data Token = None | X | O 
+  deriving Eq
+
+- |The coordinates of a square. 
+type Square = (Int,Int) 
+
+-- |A noughts and crosses board. 
+type Board = Array Square Token 
+
+-- |Returns an empty 'Board'. 
+newBoard :: Board 
+newBoard = listArray ((1,1),(3,3)) (repeat None) 
+
+-- |Puts a 'Token' in a 'Square'. 
+setSquare :: Board -> Square -> Token -> Board 
+setSquare board square token = 
+  if (board ! square) /= None then error $ "square " ++ show square ++ " is not empty" 
+  else board // [(square, token)]
