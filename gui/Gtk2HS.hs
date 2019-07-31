07a80b51fde37829ae160e7e3c02b149edc23d53
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 02:14:43 -0400

    buttons

--- gui/Gtk2HS.hs
+++ gui/Gtk2HS.hs
@@ -7,3 +7,16 @@ import OX
 main = do 
      --create a new game
      game <- newIORef newGame
+     
+     --create the main window
+     initGUI
+     window <- windowNew
+     set window [windowTitle := "OX"]
+     onDestroy window mainQuit
+     vbox <- vboxNew False 0
+     set window [containerChild := vbox]
+     label <- labelNew $ Just "Move: X"
+     boxPackEndDefaults vbox label
+     table <- tableNew 3 3 True
+     boxPackEndDefaults vbox table
+     btns <- replicateM 3 toggleButtonNew
