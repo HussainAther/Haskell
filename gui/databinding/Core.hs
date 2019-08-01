01406db8f8a41b18a27337c9c8322758f5de678a
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 21:38:47 -0400

    bind

--- gui/databinding/Core.hs
+++ gui/databinding/Core.hs
@@ -1,2 +1,11 @@
 {-# LANGUAGE ExistentialQuantification #-}
 module Binding.Core (module Binding.Variable, Bindable, bind, Source) where
+
+import Binding.Variable
+
+-- | A data binding:
+-- @a@ is the type of the data source
+-- @a -> d@ is a function that extracts data from the source
+-- @t2 is the binding target
+-- @d -> t -> IO ()@ is a function that applies data to the target
+data Binding a = forall d t. Binding (a -> d) t (t -> d -> IO ())
