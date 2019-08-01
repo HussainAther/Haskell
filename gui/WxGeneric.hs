71c39f96d19cb9850da5c81f07ba9051171caa3d
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 21:01:43 -0400

    instance

--- gui/WxGeneric.hs
+++ gui/WxGeneric.hs
@@ -5,3 +5,10 @@ import Graphics.UI.SybWidget.MySYB
 
 data Name = Name {forename :: String, surname :: String} deriving Show
 data Student = Student {name :: Name, tutor :: Name, id :: Int} deriving Show
+
+anonymous = Name "" ""
+student = Student anonymous anonymous 0
+
+$(derive [''Name, ''Student])
+instance WxGen Name
+instance WxGen Student
