204489c73340e336ad8b8e8527b3ad5b2984cbdb
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 21:43:42 -0400

    target

--- gui/databinding/Core.hs
+++ gui/databinding/Core.hs
@@ -9,3 +9,38 @@ import Binding.Variable
 -- @t2 is the binding target
 -- @d -> t -> IO ()@ is a function that applies data to the target
 data Binding a = forall d t. Binding (a -> d) t (t -> d -> IO ())
+
+-- | A simple binding source.
+data Source v a = Variable v => Source {bindings :: v [Binding a] -- ^ the source's bindings
+                                        ,var     :: v a}          -- ^ the bound variable
+
+-- | Update a single binding.
+update' :: a -> Binding a -> IO ()
+update' source (Binding extract target apply) = apply target $ extract source
+
+-- | Update a binding source's bindings.
+update :: Source v a -> IO ()
+update (Source bindings source) = do bindings <- readVar bindings
+                                     a <- readVar source
+                                     mapM_ (update' a) bindings
+
+instance Variable v => Variable (Source v) where
+   newVar a = do bindings <- nerVar []
+                 v <- newVar a
+                 return $ Source bindigns v
+
+   readVAr = readVar . var
+   writeVar s a = writeVar (var s) a >> update s
+   modifyVar s f = modifyVar (var s) f >> update s
+   modifyVar' s f = do b <- modifyVar' (var s) f
+                       update s
+                       return b
+
+-- | Binding sources.
+class Variable b => Bindable b where
+   -- | Create a data binding.
+   bind :: b a -- ^ the binding source
+         -> (a -> d) -- ^ a function that extracts data from the soucre
+         -> t -- ^ the binding target
+         -> (t -> d -> io ()) -- ^ a function that applies data to the target
+         -> (IO ()
