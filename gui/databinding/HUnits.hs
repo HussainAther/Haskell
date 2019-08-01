2a72d7ce4ff264f06bae7c8f42d73ca5b116871f
Syed Hussain Ather <shussainather@gmail.com>
2019-08-01 01:34:33 -0400

    type

--- gui/databinding/HUnits.hs
+++ gui/databinding/HUnits.hs
@@ -4,4 +4,11 @@ import Test.HUnit
 import Control.Monad
 import Data.IORef
 import System.Exit
-import System.Random 
+import System.Random
+
+import Binding.List as B
+import Prelude as P
+
+-- Change these to exercise different variable and data types
+type V = IORef
+type A = Int 
