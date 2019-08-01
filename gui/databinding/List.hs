ea3ec6effd86d417da8dbd618c6e05620af240a1
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 21:47:11 -0400

    import

--- gui/databinding/List.hs
+++ gui/databinding/List.hs
@@ -1,1 +1,8 @@
 {-# LANGUAGE ExistentialQuantification #-}
+module Binding.List (module Binding.Core, BindingList, toBindingList, fromBindingList, length, position, seek, seekBy, next, prev, remove', remove, insert' insert) where
+
+import Prelude hiding (length)
+import qualified Prelude as P
+import Control.Monad
+
+import Binding.Core
