f969d5a056a1f813c1045009317a49977bf22d77
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 21:51:04 -0400

    update

--- gui/databinding/List.hs
+++ gui/databinding/List.hs
@@ -11,3 +11,18 @@ import Binding.Core
 data BindingList v a = Variable v => BindingList {source :: Source v a -- ^ the list's binding source
                                                   ,list  :: v [v a] -- ^ the bound list
                                                   ,pos   :: v Int}  -- ^ the current position
+-- [v a] is itself in a Variable to allow for insertions and deletions
+
+-- | Create a binding list.
+toBindingList :: Variable v => [a] -> IO (BindingList v a)
+toBindingList [] = error "empty list"
+toBindingList list = do list'<- mapM newVar list >>= newVar
+                        source <- newVar (head list)
+                        pos <- newVar 0
+                        return $ BindingList source list' pos
+
+-- | Update the binding list from the 'source'.
+update :: BindingList v a -> IO ()
+update (BindingList source list pos) = do list' <- readVar list
+                                          pos' <- readVar pos
+                                          readVar source >>= writeVar (list' !! pos')
