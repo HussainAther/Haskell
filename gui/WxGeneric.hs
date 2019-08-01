4a16be9cca514197d03989763bb9f2fb12019681
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 20:59:42 -0400

    data

--- gui/WxGeneric.hs
+++ gui/WxGeneric.hs
@@ -2,3 +2,6 @@
 import Graphics.UI.WX
 import Graphics.UI.WxGeneric
 import Graphics.UI.SybWidget.MySYB
+
+data Name = Name {forename :: String, surname :: String} deriving Show
+data Student = Student {name :: Name, tutor :: Name, id :: Int} deriving Show
