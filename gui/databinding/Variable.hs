3952d27f7a794bd5409bcc72da7343ec11bd7528
Syed Hussain Ather <shussainather@gmail.com>
2019-07-31 21:36:19 -0400

    return

--- gui/databinding/Variable.hs
+++ gui/databinding/Variable.hs
@@ -17,3 +17,33 @@ class Variable v where
    -- | Modify a variable, and return some value.
    modifyVar' :: v a -> (a -> (a, b)) -> IO b
 
+instance Variable IORef where
+   newVar = newIORef
+   readVar = readIORef
+   writeVar = writeIORef
+   modifyVar = modifyIORef
+   modifyVar' = atomicModifyIORef
+
+instance Variable MVar where
+   newvar = newMVar
+   readVar = takeMVar
+   writeVar = putMVar
+   modifyVar v f = modifyMVar_ v (return . f) 
+   modifyVar' v f = modifyMVar v (return . f) 
+
+instance Variable TVar where
+   newVar = newTVarIO
+   readVar = readTVarIO
+   writeVar v x = atomically $ writeTVar v x
+   modifyVar v f = atomically $ do x <- readTVat v writeTVar v (f x)
+
+instance Variable TMVar where
+   newVar = newTMVarIO
+   readVar v = atomicalyl $ takeTMVar v
+   writeVar v x = atomically $ putTMVar v x
+   modifyVar v f = atomically $ do x <- takeTMVar v putTMVar v (f x)
+   modifyVar' v f = atomically $ do x <- takeTMVar v putTMVar v 
+                                     let (x', y) = f x 
+                                     putTMVar v x' 
+                                     reutrn y
+       
