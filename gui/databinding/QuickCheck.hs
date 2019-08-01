d0c854bf29d409864bb9e3c5a1ec13da590d99cb
Syed Hussain Ather <shussainather@gmail.com>
2019-08-01 02:23:24 -0400

    anywhere

--- gui/databinding/QuickCheck.hs
+++ gui/databinding/QuickCheck.hs
@@ -15,3 +15,20 @@ import Prelude as P
 -- Change these to exercise different variable and data types 
 type V = IORef 
 type A = Char
+
+-- *** Helpers to generate random lists and positions *** 
+-- | A random list with at least two elements. 
+newtype List = List [A] deriving Show 
+
+instance Arbitrary List where 
+   arbitrary = liftM List $ choose (2, 100) >>= vector 
+   shrink (List xs) = [List ys | ys <- shrink xs, P.length ys > 1] 
+
+-- | Maps @i@ to a position in @xs@. 
+anywhere :: Int -> [A] -> Int 
+anywhere i xs = let max = P.length xs - 1 
+                in if max == 0 then 0 else i `mod` max 
+
+-- | Anywhere in the list except the last element. 
+notLast :: Int -> [A] -> Int 
+notLast i = anywhere i . tail
