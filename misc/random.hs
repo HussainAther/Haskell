5362430b62e71a351faaa9d16a5e19b435a975c6
Syed Hussain Ather <shussainather@gmail.com>
2019-09-03 09:14:58 -0700

    values

--- misc/random.hs
+++ misc/random.hs
@@ -3,3 +3,8 @@ nextRand n = (multiplier*n + increment) 'mod' modulus
 
 randomSequence :: Int -> [Int]
 randomSequence = iterate nextRand
+
+seed       = 1234
+multiplier = 5678
+increment  = 1357
+modulus    = 2468
