38fe076163b5e30ea74592a213d6149bb083342e
Syed Hussain Ather <shussainather@gmail.com>
2019-09-06 10:01:09 -0700

    window

--- game/graphics.hs
+++ game/graphics.hs
@@ -1,3 +1,9 @@
 import Graphics.Gloss.Rendering
 import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Data.Picture
+
+main :: IO ()
+main = do
+   let width  = 640
+       height = 480
+   withWindow width height "Resurrection" $ \win -> do
