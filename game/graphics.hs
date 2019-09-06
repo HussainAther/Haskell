90005bca3d90ceed5daa43d2885236e8a8f2987b
Syed Hussain Ather <shussainather@gmail.com>
2019-09-06 10:06:02 -0700

    circle

--- game/graphics.hs
+++ game/graphics.hs
@@ -14,3 +14,12 @@ main = do
          [ Color violet $ translate (-200) 100 $ 
                polygon [((-10), 10), ((-10), 70), (20, 20), (20, 30)]
          , Color red $ translate (-200) 100 $
+               line [(-30, -30), (-40, 30), (30, 40), (50, -20)]
+         , Color (makeColor 0 128 255 1) $ translate (-100) 100 $
+               lineLoop [(-30, -30), (-40, 30), (30, 40), (50, -20)]
+         , Color red $ translate 0 100 $
+               circle 30
+         , Color green $ translate 100 100 $
+               thickCircle 30 10
+         , Color yellow $ translate 200 100 $
+               circleSolid 30
