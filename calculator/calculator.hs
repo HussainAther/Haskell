8590a2fe7eeaa2d403a36ae308d47ee046f11b0b
Syed Hussain Ather <shussainather@gmail.com>
2019-09-07 15:05:53 -0700

    store val

--- calculator/calculator.hs
+++ calculator/calculator.hs
@@ -1,3 +1,6 @@
 data Expr = Lit Int | Var Var | Op Ops Expr Expr
 data Ops  = Add | Sub | Mul | Div | Mod
 type Var  = Char
+initial :: Store
+value   :: Store -> Var -> Int
+update  :: Store -> Var -> Int -> Store
