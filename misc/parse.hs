1cbbd1b0572836c779561e165962424dcf515d64
Syed Hussain Ather <shussainather@gmail.com>
2019-10-03 20:04:09 -0700

    done

--- misc/parse.hs
+++ misc/parse.hs
@@ -16,3 +16,17 @@ spot p (x:xs)
   | p x          = [(x, xs)]
   | otherwise    = [] 
 spot p []        = []
+
+alt :: Parse a b -> Parse a b -> Parse a b
+alt pl p2 inp = pl inp ++ p2 inp
+
+(>*>) :: Parse a b -> Parse a c -> Parse a (b,c) 
+(>*>I pl p2 inp
+  = [((y,~,)re11121 I (y,rernl) <- pl inp , (z,rem2) <- p2 remi 1 
+
+build :: Parse a b -> (b -> c) -> Parse a c
+build p f inp = [ (f x,rem) I (x,rern) <- p inp ]
+
+list :: Parse a b -> Parse a [b]
+list p = (succeed []) 'alt'
+         ((p >*> list p) 'build' (uncurry (:)))
