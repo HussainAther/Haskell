7a889a2412ead668a0db5127b9ae3171a83db062
Syed Hussain Ather <shussainather@gmail.com>
2019-10-14 21:55:07 -0700

    depthlist

--- search/dfs.hs
+++ search/dfs.hs
@@ -1,3 +1,5 @@
 -- Depth-first search
 depthSearch :: Ord a => Relation a -> a -> [a] -> [a]
 depthFirst rel v = depthSearch rel v []
+depthSearch rel v used
+        = v : depthList rel (findDescs rel used' v) used'
