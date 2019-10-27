ea88f512cf9db12900fa7f56c4a66565c9eb0999
Syed Hussain Ather <shussainather@gmail.com>
2019-10-27 15:22:12 -0700

    monadic

--- mathematics/numbertree.hs
+++ mathematics/numbertree.hs
@@ -1,4 +1,12 @@
 --Sum a tree of integers
+--Recursive solution
 sTree ::TreeInt-> Int
-sTree N i l = 0
-sTree (Nodentlt2)=n+ sTree tl + sTree t2
+sTree Nil = 0
+sTree (Nodentlt2) = n + sTree t1 + sTree t2
+--Monadic solution
+sumTree :: Tree Int -> St Int
+sumTree Nil = return 0
+sumTree (Node n tl t2) 
+  = do num <- return n
+       s1 <- sumTreeti
+       s2 <- sumTreet2 return (num + s1 + s2)
