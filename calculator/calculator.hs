bfec2195802e032a5338197da00af37797603755
Syed Hussain Ather <shussainather@gmail.com>
2019-10-27 15:12:18 -0700

    cmmand

--- calculator/calculator.hs
+++ calculator/calculator.hs
@@ -9,3 +9,12 @@ commLine :: String -> Command
 commLine "(3+x)"   = (Eval (Op Add (Lit 3) ( Var 'x')))
 commLine "x:(3+x)" = (Assign 'x' (Op Add (Lit 3) (Var 'x')))
 commLine ""        = Null
+command : : Command -> Store -> (Int,Store)
+command Null st = ( 0 , st) 
+command (Eva1 e) st = (eval e st , st) 
+command (Assign v e) st
+  = (val , newst) 
+     where
+     val = evalest 
+     newst = update st v val
+
