68b73dac693e1e62066a5096e46f8fee0a3c83a3
Syed Hussain Ather <shussainather@gmail.com>
2019-11-29 14:52:28 -0800

    weigh the dist

--- computerscience/bayesrule.hs
+++ computerscience/bayesrule.hs
@@ -1,14 +1,14 @@
---If we were administering drug tests for a small business,
---we could represent each employee's test results and study
---them using Bayes' (bayes) rule.
+-- If we were administering drug tests for a small business,
+-- we could represent each employee's test results and study
+-- them using Bayes' (bayes) rule.
 
 data Test = Pos | Neg
   deriving (Show, Eq)
 
 data HeroinStatus = User | Clean
   deriving (Show, Eq)
---If .1% of employees have recently used heroin and the test
---is 99% accurate, we can model the test.
+-- If .1% of employees have recently used heroin and the test
+-- is 99% accurate, we can model the test.
 drugTest1 :: Dist d => d (HeroinStatus, Test)
 drugTest1 = do
   heroinStatus <- percentUser 0.1
@@ -18,3 +18,10 @@ drugTest1 = do
       else percentPos 1
   return (heroinStatus, testResult)
 
+-- Some handy distributions.
+percentUser p = percent p User Clean
+percentPos p = percent p Pos Neg
+
+-- A weighted distribution with two elements.
+percent p x1 x2 =
+  weighted [(x1, p), (x2, 100-p)]
