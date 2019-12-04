606f133022fa611cd139b656cc6aa4b30f715e71
Syed Hussain Ather <shussainather@gmail.com>
2019-12-03 20:31:08 -0800

    in it! in it1

--- mathematics/feigenbaum.hs
+++ mathematics/feigenbaum.hs
@@ -6,3 +6,12 @@ feigenbaumApprox :: Int -> [Double]
 feigenbaumApprox mx = snd $ mitch mx 10
   where
     mitch :: Int -> Int -> ((Double, Double, Double), [Double])
+    mitch mx mxj =
+      mapAccumL
+        (\(a1, a2, d1) i ->
+            let a =
+                  iterate
+                    (\a ->
+                        let (x, y) =
+                              iterate
+                                (\(x, y) -> (a - (x * x), 1.0 - ((2.0 * x) * y)))
