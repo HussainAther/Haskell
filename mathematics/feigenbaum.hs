168bc726c47c2b07ba2e96f1395f4a13d8161e02
Syed Hussain Ather <shussainather@gmail.com>
2019-12-03 20:29:34 -0800

    feigenbaum

--- mathematics/feigenbaum.hs
+++ mathematics/feigenbaum.hs
@@ -1,3 +1,8 @@
 import Data.List (mapAccumL)
 
 -- Feigenbaum constants
+
+feigenbaumApprox :: Int -> [Double]
+feigenbaumApprox mx = snd $ mitch mx 10
+  where
+    mitch :: Int -> Int -> ((Double, Double, Double), [Double])
